f0c76fb0e48f7bf825316f1549136809bea1794f
Brandon Sandoval <brandon.sandoval123@gmail.com>
2023-01-16 23:04:13 -0600

    added spacing for clarity

--- Converter_App/Convert.py
+++ Converter_App/Convert.py
@@ -21,21 +21,29 @@ while True:
         break
 
     if event == "-CONVERT-":
+        
         input_value = values['-INPUT-']
         
         if input_value.isnumeric():
+
+            window["-OUTDISPLAY-"].update(visible=True)
+
             match values['-UNITS-']:
+
                 case 'km to mile':
+
                     output = round(float(input_value)) * 0.6214
                     output_string = f'{input_value} kilometers -> {output} miles'
-                    window['-OUTPUT-'].update(output_string)
+                    window['-OUTPUT-'].update(output_string)    
                 
                 case 'kg to pound':
+
                     output = round(float(input_value)) * 2.20462
                     output_string = f'{input_value} kilograms -> {output} pounds'
                     window['-OUTPUT-'].update(output_string)
                 
                 case 'sec to min':
+
                     output = round(float(input_value)) / 60
                     output_string = f'{input_value} seconds -> {output} mins'
                     window['-OUTPUT-'].update(output_string)
@@ -44,7 +52,7 @@ while True:
 
         
         else:
-            window[]
+            window["-OUTDISPLAY-"].update(visible=False)
             window['-OUTPUT-'].update("Please enter a number!")
 
         
