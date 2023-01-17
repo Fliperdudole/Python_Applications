1cbad963296b44c400c80d431b148b59f9b2ec4a
Brandon Sandoval <brandon.sandoval123@gmail.com>
2023-01-16 22:51:28 -0600

    Added the conversion method

--- Converter_App/Convert.py
+++ Converter_App/Convert.py
@@ -1,9 +1,15 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Input(key = "-INPUT-"), sg.Spin(["km to mile", "kg to pound"]), sg.Button("Convert")],
+    [
+        sg.Input(key = "-INPUT-"), 
+        sg.Spin(["km to mile", "kg to pound", "sec to min"], key = '-UNITS-'), 
+        sg.Button("Convert", key = "-CONVERT-")
+    ],
 
-    [sg.Text("Output:"), sg.Text(" ", enable_events= True, key="-OUTPUT-")]
+    [
+        sg.Text("Output:"), sg.Text(" ", enable_events= True, key="-OUTPUT-")
+    ]
 ]
 
 window = sg.Window('Converter', layout)
@@ -14,8 +20,33 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
-    if event == "Convert":
-        window['-OUTPUT-'].update(values['-INPUT-'])
+    if event == "-CONVERT-":
+        input_value = values['-INPUT-']
+        
+        if input_value.isnumeric():
+            match values['-UNITS-']:
+                case 'km to mile':
+                    output = round(float(input_value)) * 0.6214
+                    output_string = f'{input_value} kilometers -> {output} miles'
+                    window['-OUTPUT-'].update(output_string)
+                
+                case 'kg to pound':
+                    output = round(float(input_value)) * 2.20462
+                    output_string = f'{input_value} kilograms -> {output} pounds'
+                    window['-OUTPUT-'].update(output_string)
+                
+                case 'sec to min':
+                    output = round(float(input_value)) / 60
+                    output_string = f'{input_value} seconds -> {output} mins'
+                    window['-OUTPUT-'].update(output_string)
+
+
+
+        
+        else:
+            print("Enter a number!")
+
+        
     
 
 
