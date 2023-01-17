efde13f5f2a151978d3fabda77b0c5f6451a2c5b
Brandon Sandoval <brandon.sandoval123@gmail.com>
2023-01-16 22:09:03 -0600

    Added Testing Buttons

--- Converter_App/Convert.py
+++ Converter_App/Convert.py
@@ -2,9 +2,12 @@ import PySimpleGUI as sg
 
 layout = [
     [sg.Text("Text"), sg.Spin(['Kilometers','Meters'])],
-    [sg.Button("Convert")],
+
+    [sg.Button("Convert", key = "-BUTTON1-")],
+
     [sg.Input()],
-    [sg.Text("Test Button"), sg.Button("Test Button")]
+    
+    [sg.Text("Test Button"), sg.Button("Button", key = "-BUTTON2-")]
 ]
 
 window = sg.Window('Converter', layout)
@@ -14,5 +17,12 @@ while True:
 
     if event == sg.WIN_CLOSED:
         break
+
+    if event == "-BUTTON1-":
+        print("Button Pressed")
+
+    if event == "-BUTTON2-":
+        print("Test Button Pressed")
+
 
 window.close()
